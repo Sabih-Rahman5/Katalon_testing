cde95e0a4bca15a8e0cb940104a988887c1c4926
Sabih Rahamn <sabih.562@gmail.com>
2023-07-05 10:54:36 +0500

    masood
    Change-Id: I8a384612f7e3cf0ab74fba7a71e65249bc35db63

---
+++ Test Suites/Rest Soap requests.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Rest Soap requests</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>fe7a5c8b-3ef5-4d7a-8e7a-ce936bf89c71</testSuiteGuid>
+   <testCaseLink>
+      <guid>12043595-d84f-465f-9db4-6dc218763c5f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SQE rest_Soap requests</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
